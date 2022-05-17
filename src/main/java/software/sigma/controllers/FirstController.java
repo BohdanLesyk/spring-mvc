b1a79a458c56e129d30d17c6cefae3ac058f23b9
Bohdan Lesyk <bohdan.lesyk@sigma.software>
2022-05-17 23:52:47 +0300

    Revert "Lesson 19. Parameters from GET Request. `@RequestParam` annotation"
    This reverts commit 804036b849fb3f81c0fce41753ce964b97070f8d.

--- src/main/java/software/sigma/controllers/FirstController.java
+++ src/main/java/software/sigma/controllers/FirstController.java
@@ -1,7 +1,6 @@
 package software.sigma.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,11 +11,11 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("hello")
-    public String helloPage(HttpServletRequest request, Model model) {
+    public String helloPage(HttpServletRequest request) {
         String name = request.getParameter("name");
         String surname = request.getParameter("surname");
 
-        model.addAttribute("message", "Hello, " + name + " " + surname);
+        System.out.println("Hello, " + name + " " + surname);
 
         return "first/hello";
     }
@@ -27,35 +26,5 @@ public class FirstController {
         System.out.println("Goodbye, " + name + " " + surname);
 
         return "first/goodbye";
-    }
-
-    @GetMapping("/calculate")
-    public String calculate(@RequestParam("firstParam") int firstParam,
-                            @RequestParam("secondParam") int secondParam,
-                            @RequestParam("action") String action,
-                            Model model) {
-        double result;
-
-        switch (action) {
-            case "multiplicate":
-                result = firstParam * secondParam;
-                break;
-            case "division":
-                result = firstParam / (double)secondParam;
-                break;
-            case "subtraction":
-                result = firstParam - secondParam;
-                break;
-            case "addition":
-                result = firstParam + secondParam;
-                break;
-            default:
-                result = 0;
-                break;
-        }
-
-        model.addAttribute("result", result);
-
-        return "first/calculate";
     }
 }

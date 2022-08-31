b4b925414d142c2ba3bbd95149ecff5a413f97db
Bohdan Lesyk <b0gdan41k2007@gmail.com>
2022-08-31 19:30:45 +0300

    Lesson 25. JDBC API. Add DB PostgreSQL dependency. Refactor `index()` and `create()` methods

--- src/main/java/software/sigma/UserDAO.java
+++ src/main/java/software/sigma/UserDAO.java
@@ -3,45 +3,75 @@ package software.sigma;
 import org.springframework.stereotype.Component;
 import software.sigma.models.User;
 
-import java.util.LinkedList;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component  // Bean
 public class UserDAO {
-    private static int USERS_COUNT;
-    private List<User> users;
+    private static final String CONNECTION_URL = "jdbc:postgresql://localhost:5432/postgres";//Here all configs would
+    private static final String USERNAME = "postgres";//be better save in properties file in real projects
+    private static final String PASSWORD = "postgres";
 
-    {
-        users = new LinkedList<>();
+    private static Connection connection;
 
-        users.add(new User(++USERS_COUNT, "Tom", 24, "tom@gmail.com"));
-        users.add(new User(++USERS_COUNT, "Bred", 53, "bred@gmail.com"));
-        users.add(new User(++USERS_COUNT, "Jolie", 18, "jolie@gmail.com"));
-        users.add(new User(++USERS_COUNT, "John", 35, "john@gmail.com"));
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            connection = DriverManager.getConnection(CONNECTION_URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> index() {
+        List<User> users = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String query = "SELECT * FROM users";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                User user = new User();
+
+                user.setId(resultSet.getInt("id"));
+                user.setName(resultSet.getString("name"));
+                user.setAge(resultSet.getInt("age"));
+                user.setEmail(resultSet.getString("email"));
+
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return users;
     }
 
     public User show (final int userId) {
-        return users.stream().filter(user -> user.getId() == userId).findAny().orElse(null);
+        return null;
     }
 
     public void save(User user) {
-        user.setId(++USERS_COUNT);
-        users.add(user);
+        try {
+            Statement statement = connection.createStatement();
+
+            String query = "INSERT INTO users VALUES(" + 1 + ",'" + user.getName() + "'," +
+                user.getAge() + ",'" + user.getEmail() + "');";
+
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void update(int id, User user) {
-        User userToBeUpdated = show(id);
+    public void update(int id, User user) { }
 
-        userToBeUpdated.setName(user.getName());
-        userToBeUpdated.setAge(user.getAge());
-        userToBeUpdated.setEmail(user.getEmail());
-    }
-
-    public void delete(int id) {
-        users.removeIf(user -> user.getId() == id);
-    }
+    public void delete(int id) { }
 }
